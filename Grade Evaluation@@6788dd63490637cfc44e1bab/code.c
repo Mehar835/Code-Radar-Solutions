0199434ded72580a017b097b15030bf293d4e2c7
Mehar835 <meharsofat@gmail.com>
2025-02-15 16:19:04 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -15,9 +15,8 @@ int main(){
     else if(a=='D'){
        printf("Below Average");
     }
-    else if(a=='E'){
+    else if(a=='F'){
       printf("Fail");
-    }
     else{
         printf("Invalid grade");
     }
