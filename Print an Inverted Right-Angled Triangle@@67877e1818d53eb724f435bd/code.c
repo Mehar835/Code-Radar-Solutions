090eb6100dbace5c7a562c9cefe9ef184a0ef2b7
Mehar835 <meharsofat@gmail.com>
2025-02-16 11:16:30 +0530

    Solution to Print an Inverted Right-Angled Triangle

--- Print an Inverted Right-Angled Triangle@@67877e1818d53eb724f435bd/code.c
+++ Print an Inverted Right-Angled Triangle@@67877e1818d53eb724f435bd/code.c
@@ -1,49 +1,69 @@
-// Your code here...
+// // Your code here...
+// #include<stdio.h>
+// int main(){
+//     int a;
+//     scanf("%d",&a);
+//     if(a==1){
+//         printf("*");
+//     }
+//     else if(a==2){
+//         printf("* *\n");
+//         printf("*");
+//     }
+//     else if(a==3){
+//         printf("* * *\n");
+//         printf("* *\n");
+//         printf("*");
+//     }
+//     else if(a==4){
+//         printf("* * * *\n");
+//         printf("* * *\n");
+//         printf("* *\n");
+//         printf("*");
+//     }
+//     else if(a==5){
+//         printf("* * * * *\n");
+//         printf("* * * *\n");
+//         printf("* * *\n");
+//         printf("* *\n");
+//         printf("*");
+//     }
+//     else if(a==6){
+//         printf("* * * * * *\n");
+//         printf("* * * * *\n");
+//         printf("* * * *\n");
+//         printf("* * *\n");
+//         printf("* *\n");
+//         printf("*");
+//     }
+//     else if(a==7){
+//         printf("* * * * * * *\n"); 
+//         printf("* * * * * *\n"); 
+//         printf("* * * * *\n");
+//         printf("* * * *\n");
+//         printf("* * *\n");
+//         printf("* *\n");
+//         printf("*");
+//     }
+    
+// }
 #include<stdio.h>
 int main(){
-    int a;
+    int a, i;
     scanf("%d",&a);
-    if(a==1){
-        printf("*");
-    }
-    else if(a==2){
-        printf("* *\n");
-        printf("*");
-    }
-    else if(a==3){
-        printf("* * *\n");
-        printf("* *\n");
-        printf("*");
-    }
-    else if(a==4){
-        printf("* * * *\n");
-        printf("* * *\n");
-        printf("* *\n");
-        printf("*");
-    }
-    else if(a==5){
-        printf("* * * * *\n");
-        printf("* * * *\n");
-        printf("* * *\n");
-        printf("* *\n");
-        printf("*");
-    }
-    else if(a==6){
-        printf("* * * * * *\n");
-        printf("* * * * *\n");
-        printf("* * * *\n");
-        printf("* * *\n");
-        printf("* *\n");
-        printf("*");
-    }
-    else if(a==7){
-        printf("* * * * * * *\n"); 
-        printf("* * * * * *\n"); 
-        printf("* * * * *\n");
-        printf("* * * *\n");
-        printf("* * *\n");
-        printf("* *\n");
-        printf("*");
-    }
+   for (i=a,i>=1,i=i-1){
+    printf("*\n"*i);
+   }
     
-}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
