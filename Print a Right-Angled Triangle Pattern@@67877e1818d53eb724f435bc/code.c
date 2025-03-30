3ac58eb00a65516c22735e1c8de498b8ce72d078
Mehar835 <meharsofat@gmail.com>
2025-03-30 09:44:13 +0530

    Solution to Print a Right-Angled Triangle Pattern

--- Print a Right-Angled Triangle Pattern@@67877e1818d53eb724f435bc/code.c
+++ Print a Right-Angled Triangle Pattern@@67877e1818d53eb724f435bc/code.c
@@ -1,12 +1,58 @@
 // Your code here...
-#include<stdio.h>
+// #include<stdio.h>
+// int main(){
+//     int a, i,j;
+//     scanf("%d",&a);
+//    for (i=1;i<=a;i=i+1){
+//    for (j=1;j<=i;j=j+1){
+//     printf("* ");
+//    }
+//     printf("\n");
+//    }
+//     }
+# include <stdio.h>
 int main(){
-    int a, i,j;
+    int a;
     scanf("%d",&a);
-   for (i=1;i<=a;i=i+1){
-   for (j=1;j<=i;j=j+1){
-    printf("* ");
-   }
-    printf("\n");
-   }
+    if(a==1){
+        printf("*");
     }
+    else if(a==2){
+        printf("*");
+        printf("* *")
+    }
+    else if(a==3){
+        printf("*");
+        printf("* *")
+        printf("* * *")
+    }
+    else if(a==4){
+        printf("*");
+        printf("* *")
+        printf("* * *")
+        printf("* * * *")
+    }
+    else if(a==5){
+        printf("*");
+        printf("* *")
+        printf("* * *")
+        printf("* * * *")
+        printf("* * * * *")
+    else if(a==6){
+        printf("*");
+        printf("* *")
+        printf("* * *")
+        printf("* * * *")
+        printf("* * * * *")
+        printf("* * * * * *")
+    }
+    else if(a==7){
+        printf("*");
+        printf("* *")
+        printf("* * *")
+        printf("* * * *")
+        printf("* * * * *")
+        printf("* * * * * *")
+        printf("* * * * * * *")
+    }
+}}
