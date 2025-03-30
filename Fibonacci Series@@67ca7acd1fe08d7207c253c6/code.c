57441a0f0a9ef669d7e27e444785ce5b58106f64
Mehar835 <meharsofat@gmail.com>
2025-03-30 16:26:50 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,34 @@
+// Your code here...
+#include <stdio.h>
+
+// You
+
+esc
+
+?
+
+12
+
+int main() {
+
+int n;
+
+// Read the number of terms
+
+scanf("%d", &n);
+
+// Validate input
+
+if (n <= 0) {
+
+printf("Invalid input\n");
+
+return 0;
+
+}
+
+// Call the fibonacciSeries function (to be implemented by the student)
+
+fibonacciSeries(n); A
+
+return 0;
